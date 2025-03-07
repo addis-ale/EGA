c435fb7a9784da73299ba9bdee6cfdc4fe4d0fec
Addis <addis.ugr-4143-15@aau.edu.et>
2025-03-08 00:03:50 +0300

    feat: implement product creation API with validation, error handling, and video upload support

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,8 +1,78 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import prisma from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 import { getCurrentUser } from "@/actions/getCurrentUser";
+import { ZodError } from "zod";
+import { productSchema } from "@/schemas/productSchema";
 
 type SortOrder = "asc" | "desc";
+export async function POST(req: Request, res: Response) {
+  try {
+    const body = await req.json();
+    const validatedData = productSchema.parse(body);
+    const {
+      productName,
+      productDescription,
+      uploadedCoverImage,
+      discountPercentage,
+      ageRestriction,
+      gameType,
+      availableForSale,
+      availableForRent,
+      productType,
+      pricing: {
+        salePrice,
+        rentalPricePerHour,
+        minimumRentalPeriod,
+        maximumRentalPeriod,
+      },
+      uploadedVideo: { setUp, actionCard, gamePlay },
+    } = validatedData;
+
+    const newProduct = await prisma.product.create({
+      data: {
+        productName,
+        productDescription,
+        uploadedCoverImage,
+        discountPercentage: discountPercentage ?? 0,
+        ageRestriction,
+        gameType,
+        availableForSale: availableForSale ?? 0,
+        availableForRent: availableForRent ?? 0,
+        productType,
+      },
+    });
+
+    const newPriceDetails = await prisma.priceDetails.create({
+      data: {
+        productId: newProduct.id,
+        salePrice,
+        rentalPricePerHour,
+        minimumRentalPeriod,
+        maximumRentalPeriod,
+      },
+    });
+    const newVideoUpload = await prisma.videoUploaded.create({
+      data: {
+        productId: newProduct.id,
+        setUp,
+        actionCard,
+        gamePlay,
+      },
+    });
+    return NextResponse.json(
+      {
+        success: true,
+        product: newProduct,
+        priceDetails: newPriceDetails,
+        uploadedVideo: newVideoUpload,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return handleError(error, res);
+  }
+}
 
 export async function GET(req: Request) {
   try {
@@ -179,4 +249,26 @@ export async function GET(req: Request) {
       { status: 500 }
     );
   }
+}
+function handleError(error: unknown, res: Response) {
+  console.error("API Error:", error);
+
+  if (error instanceof ZodError) {
+    return NextResponse.json(
+      { success: false, errors: error.errors },
+      { status: 400 }
+    );
+  }
+
+  if (error instanceof Error) {
+    return NextResponse.json(
+      { success: false, message: "Record not found" },
+      { status: 403 }
+    );
+  }
+
+  return NextResponse.json(
+    { success: false, message: "Server Error" },
+    { status: 500 }
+  );
 }
